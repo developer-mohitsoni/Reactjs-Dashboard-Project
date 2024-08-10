a82231fe3efcfca16d3006eac5b243b0baf28c53
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-10 16:19:56 +0530

    Update npm dependencies and add React Query for API data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 60 * 1000 * 5,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
-  </StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,9 +9,23 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLoginSubmit = () => {
+    // This will give me my value that we are enter on input form login box
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    console.log("data", { email, password });
+
+    // Make server call.
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -25,6 +39,7 @@ const LoginPage = () => {
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              ref={emailRef}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -33,12 +48,14 @@ const LoginPage = () => {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
-            <Button className="w-full">Sign in</Button>
+            <Button className="w-full" onClick={handleLoginSubmit}>
+              Sign in
+            </Button>
 
             <div className="mt-4 text-center text-sm">
               Create an account?{" "}

67e06d59c97dc66c1bd748a6917dc3dc89269146
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-10 14:07:01 +0530

    Setup Shadcn Ui into Our Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
   return (
     <>
       <h1>Welcome to Reactjs Dashboard</h1>
+      <Button>Click Me</Button>
     </>
   );
 };
